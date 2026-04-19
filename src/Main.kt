8e660334b208c4dce92014fd98a722ef9b82640c
Sliceeee01 <ivan_hih@icloud.com>
2026-04-19 20:00:32 +0300

    Create sealed model ModuleResult

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,21 @@
-import resources.OutpostResource
+import modules.ModuleResult
+import modules.EnergyGenerator
+import modules.ResearchLab
 import resources.ResourceManager
+import resources.OutpostResource
+
+fun handleModuleResult(result: ModuleResult) {
+    when (result) {
+        is ModuleResult.Success -> println("УСПЕХ: ${result.message}")
+        is ModuleResult.ResourceProduced -> println("Произведено: ${result.resourceName} +${result.amount}")
+        is ModuleResult.NotEnoughResources -> println(
+            "Недостаточно ресурса ${result.resourceName}. " +
+                    "Нужно: ${result.required}, есть: ${result.available}"
+        )
+        is ModuleResult.Error -> println("ОШИБКА: ${result.reason}")
+    }
+}
+
 
 fun main() {
     val manager = ResourceManager()
